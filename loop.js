72fb7d83db1fe45896a79f7f4698d4ce6117d7c9
MrNam1997 <nammaco997@gmail.com>
2019-11-15 15:54:27 +0700

    for loop tyscript

---
+++ loop.js
@@ -0,0 +1,12 @@
+var arrs = ['nam', 'nu', 'bede'];
+// for su dung of
+for (var _i = 0, arrs_1 = arrs; _i < arrs_1.length; _i++) {
+    var arr = arrs_1[_i];
+    console.log(arr);
+}
+;
+// for su dung in
+for (var arr in arrs) {
+    console.log(arr);
+}
+;
---
+++ loop.ts
@@ -0,0 +1,9 @@
+let arrs = ['nam', 'nu', 'bede'];
+// for su dung of
+for (let arr of arrs){
+    console.log(arr);
+};
+ // for su dung in
+for (let arr in arrs){
+    console.log(arr);
+};
